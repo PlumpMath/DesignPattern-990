e1c40ad412f67e3840aa4f022590bf6bbf02070f
Dibyendu <dibyendu.tiwary@gmail.com>
2013-09-13 00:18:22 +0530

    DesignPatternCode
    Code done at home

---
+++ DesignPattern/Autofactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DesignPattern
+{
+    public class Autofactory
+    {
+
+        Dictionary<string, Type> autos;
+
+        public Autofactory()
+        {
+            LoadTypesICanReturn();
+        }
+
+        public IAuto CreateInstance(string carName)
+        {
+            Type t = GetTypeToCreate(carName);
+
+            if (t == null)
+                return new NullAuto();
+
+            return Activator.CreateInstance(t) as IAuto;
+            
+        }
+
+        private Type GetTypeToCreate(string carName)
+        {
+            foreach (var auto in autos)
+            {
+                if (auto.Key.Contains(carName))
+                    return autos[auto.Key];
+            }
+            return null;            
+        }
+
+        private void LoadTypesICanReturn()
+        {
+            autos = new Dictionary<string, Type>();
+            var types  = Assembly.GetExecutingAssembly().GetTypes().ToList();
+            types.ForEach(AddMatchedInterfaceToDictionary);
+        }
+
+        private void AddMatchedInterfaceToDictionary(Type anyTypeInAssembly)
+        {
+            if (anyTypeInAssembly.GetInterface(typeof(IAuto).ToString()) != null)
+                autos.Add(anyTypeInAssembly.Name.ToLower(), anyTypeInAssembly);
+        }
+    }
+}
---
+++ DesignPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string carName = args[0];
+
+            //Caller knows about the concrete factory type here
+            //Meaning the caller knows which factory type to be called 
+            Autofactory factory = new Autofactory();
+
+            IAuto car = factory.CreateInstance(carName);
+
+            car.TurnOn();
+            car.TurnOff();
+
+            Console.ReadKey();
+        }
+    }
+}
